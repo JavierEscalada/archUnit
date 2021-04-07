5fb0df469a25eadb0fdea51e8db1b57993b31843
Francisco Javier Escalada Sarabia <javier.escalada@vass.es>
2021-04-07 13:46:35 +0200

    first commit

---
+++ src/test/java/com/demo/archunit/ArchunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.archunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
